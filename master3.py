ec81e68b86f97b87ad4d1793304e5a73cd63e98b
elite-x1x <jhankdrat4@gmail.com>
2025-12-20 05:23:39 +0700

    1

--- master3.py
+++ master3.py
@@ -4,7 +4,8 @@ load_dotenv()
 import sys, json, random
 from datetime import datetime
 from app3.config.imports import *
-from app3.menus.util import clear_screenx
+from app2.config.theme_config import get_theme_name, format_theme_name
+from app2.menus.util import clear_screenx, mask_number
 from app3.menus.sharing import show_balance_allotment_menu
 from app3.menus.purchase import redeem_looping
 from app3.menus.family import show_family_input_menu
@@ -65,7 +66,21 @@ def show_main_menu(profile: dict, display_quota: Text | None, segments: dict):
     info_table.add_column(justify="left", style=get_theme_style("text_sub"))
     info_table.add_column(justify="left", style=get_theme_style("text_body"))
 
-    info_table.add_row(" Nomor", f":📞 [bold {theme['text_body']}]{profile['number']}[/]")
+    active_theme_name = get_theme_name()
+    formatted_theme_name = format_theme_name(active_theme_name)
+    info_table.add_row(" Tema Aktif", f":🎨 [{theme['text_sub']}]{formatted_theme_name}[/]")
+
+    masked_number = mask_number(profile['number'])
+    if account_name and account_name != "-":
+        nomor_text = (
+            f":📞  [bold {theme['text_body']}]{masked_number}[/] "
+            f"[{theme['text_body']}]| {account_name}[/]"
+        )
+    else:
+        nomor_text = f":📞 [bold {theme['text_body']}]{masked_number}[/]"
+
+    info_table.add_row(" Nomor(akun)", nomor_text)
+    #info_table.add_row(" Nomor", f":📞 [bold {theme['text_body']}]{profile['number']}[/]")
     info_table.add_row(" Type", f":🧾 [{theme['text_body']}]{profile['subscription_type']} ({profile['subscriber_id']})[/]")
     info_table.add_row(" Pulsa", f":💰 Rp [{theme['text_money']}]{pulsa_str}[/]")
 
