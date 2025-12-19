8e4d9e3625acadae5df7a90de376d22b5039aecf
elite-x1x <jhankdrat4@gmail.com>
2025-12-19 20:43:42 +0700

    1

--- app2/menus/util.py
+++ app2/menus/util.py
@@ -129,9 +129,9 @@ def simple_number():
         masked_number = mask_number(number)
         account_name = active_user.get("name", "")
         if account_name and account_name != "-":
-            text = f"[bold {theme['text_sub']}]Akun aktif: {masked_number} ({account_name})[/]"
+            text = f"[bold {theme['text_title']}]Akun aktif: {masked_number} ({account_name})[/]"
         else:
-            text = f"[bold {theme['text_sub']}]Akun aktif: {masked_number}[/]"
+            text = f"[bold {theme['text_title']}]Akun aktif: {masked_number}[/]"
 
     console.print(Panel(
         Align.center(text),
