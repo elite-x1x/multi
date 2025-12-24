1137d9d0176cec376ec9d7ec5d5de20395569856
elite-x1x <jhankdrat4@gmail.com>
2025-12-24 18:16:26 +0700

    1

--- app/client/encrypt.py
+++ app/client/encrypt.py
@@ -69,14 +69,14 @@ def load_ax_fp() -> str:
 
     # Generate new if not found/empty
     dev = DeviceInfo(
-        manufacturer="samsung" + str(randint(1000, 9999)),
-        model="SM-N93" + str(randint(1000, 9999)),
-        lang="en",
-        resolution="720x1540",
+        manufacturer="Oppo" + str(randint(1000, 9999)),
+        model="CPH2737" + str(randint(1000, 9999)),
+        lang="id",
+        resolution="1080x2412",
         tz_short="GMT07:00",
         ip="192.169.69.69",
         font_scale=1.0,
-        android_release="13",
+        android_release="14",
         msisdn="6281398370564",
     )
     new_fp = ax_fingerprint(dev, AX_FP_KEY)
